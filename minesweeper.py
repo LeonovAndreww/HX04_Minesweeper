4f2ae89ce1d81bec8b90e5b4d9c29fea78a9ac17
LeonovAndreww <andrewleonov2007@gmail.com>
2025-09-29 21:29:33 -0500

    Implemented known_mines, known_safes, mark_mine, mark_safe functions

--- minesweeper.py
+++ minesweeper.py
@@ -102,31 +102,17 @@ class Sentence():
         return f"{self.cells} = {self.count}"
 
     def known_mines(self):
-        """
-        Returns the set of all cells in self.cells known to be mines.
-        """
-        raise NotImplementedError
+        if len(self.cells) == self.count: return self.cells;
 
     def known_safes(self):
-        """
-        Returns the set of all cells in self.cells known to be safe.
-        """
-        raise NotImplementedError
+        if len(self.cells) == 0: return self.cells;
 
     def mark_mine(self, cell):
-        """
-        Updates internal knowledge representation given the fact that
-        a cell is known to be a mine.
-        """
-        raise NotImplementedError
+        self.cells.remove(cell)
+        self.count -= 1
 
     def mark_safe(self, cell):
-        """
-        Updates internal knowledge representation given the fact that
-        a cell is known to be safe.
-        """
-        raise NotImplementedError
-
+        self.cells.remove(cell)
 
 class MinesweeperAI():
     """
